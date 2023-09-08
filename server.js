66035972903f614462c37c348c795c6448b5427a
Tarasdeveloper <tomahavki1@gmail.com>
2023-09-08 18:10:24 +0300

    engines added

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+import mongoose from 'mongoose';
+
 import app from './app.js';
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+// console.log(process.env);
+
+const DB_HOST =
+  'mongodb+srv://Taras:gHd87goCBDZVwG5H@cluster0.td0ighl.mongodb.net/my-contacts?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
