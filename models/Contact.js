0ae94cf211def17ed11174e03385146e8f54e600
Tarasdeveloper <tomahavki1@gmail.com>
2023-09-09 23:46:43 +0300

    May be all

--- models/Contact.js
+++ models/Contact.js
@@ -1,9 +1,50 @@
 import { Schema, model } from 'mongoose';
+import Joi from 'joi';
+import { handleSaveError, runValidateAtUpdate } from './hooks.js';
 
-const contactSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: false }
+);
+
+contactSchema.post('save', handleSaveError);
+
+contactSchema.pre('findOneAndUpdate', runValidateAtUpdate);
+
+contactSchema.post('findOneAndUpdate', handleSaveError);
+
+export const contactsAddSchema = Joi.object({
+  name: Joi.string().required().messages({
+    'any.required': `'name' must be exist`,
+  }),
+  email: Joi.string().required(),
+  phone: Joi.string().required().messages({
+    'any.required': `'number' must be exist`,
+  }),
+  favorite: Joi.boolean(),
+});
+
+export const contactUpdateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    'any.required': `missing field favorite`,
+  }),
 });
 
 const Contact = model('contact', contactSchema);
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
+      console.log(
+        `Database connection successful. Use our API on port: ${PORT}`
+      );
     });
   })
   .catch((error) => {
